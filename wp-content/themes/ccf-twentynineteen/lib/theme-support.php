aa67a74d425f8613635dd044809fd9b2c172b524
Carl Avidano <cavidano@gmail.com>
2018-12-16 03:28:29 -0500

    adding post template

--- wp-content/themes/ccf-twentynineteen/lib/theme-support.php
+++ wp-content/themes/ccf-twentynineteen/lib/theme-support.php
@@ -39,7 +39,6 @@
     // Custom Post Types
     ////////////////////////////////////////
 
-
     add_action( 'init', __NAMESPACE__ . 'create_post_type' );
 
     function create_post_type() {
@@ -71,6 +70,31 @@
             )
         );
     }
+
+    add_action( 'init', __NAMESPACE__ . '\customize_post_object' );
+
+    function customize_post_object() {
+
+        $get_post_type = get_post_type_object('post');
+        
+        $labels = $get_post_type->labels;
+        $labels->name = 'News';
+        $labels->singular_name = 'News';
+        $labels->add_new = 'Add News';
+        $labels->add_new_item = 'Add News';
+        $labels->edit_item = 'Edit Post';
+        $labels->new_item = 'News';
+        $labels->view_item = 'View Post';
+        $labels->search_items = 'Search News';
+        $labels->not_found = 'No News found';
+        $labels->not_found_in_trash = 'No News found in Trash';
+        $labels->all_items = 'All News';
+        $labels->menu_name = 'News';
+        $labels->name_admin_bar = 'News';
+        $labels->menu_position = '10';
+    }
+
+
 
     ////////////////////////////////////////
     // Remove Dashboard Menu Items
